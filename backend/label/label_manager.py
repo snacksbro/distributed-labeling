7577751493057d42f4756707d7a5fcf4b8f7ef8f
Tyler Walker <tyler.j.walker@maine.edu>
2024-03-23 14:27:02 -0400

    docs(label): Finish class docstring

--- backend/label/label_manager.py
+++ backend/label/label_manager.py
@@ -2,10 +2,54 @@
 
 
 class LabelManager:
+    """
+    Manages labels for a given DICOM file
+
+    Parameters
+    ----------
+    slice_num (int):
+        The number of slices in the stack
+
+    Attributes
+    ----------
+    default_colors (list):
+        The default colors to be used in the event a given color isn't provided
+    labels (dict):
+        A mapping of the label name to the color used for it
+    dicom_labels (list):
+        A big object tracking the labels per each DICOM slice. See notes.
+    slice_num (int):
+        The number of slices in the DICOM stack
+
+    Notes
+    -----
+    dicom_labels looks like this:
+    [ # Each slice index...
+        [ # Each label (so if there's 5 boxes, there will be 5 here
+            { # Individual label
+                name: label_name, # The name, so you can look it up later
+                points: [ # Each point, boxes have two
+                    (0, 0), # Point 1
+                    (50, 50), # Point 2
+                ]
+            }
+        ]
+    ]
+    """
+
     def __init__(self, slice_num):
+        """
+        Initializer for LabelManager
+
+        Parameters
+        ----------
+        slice_num (int):
+            The number of slices in the DICOM stack
+        """
         self.default_colors = ["FF0000", "00FF00", "0000FF"]
         self.labels = {}  # name : hex_color
         self.dicom_labels = [None for i in range(slice_num)]
+        self.slice_num = slice_num
 
     def create_label(self, label_name, color=None):
         """
@@ -18,9 +62,8 @@ class LabelManager:
         color (str):
             (Optional) The color of the label
         """
-        # TODO: Make this based on len of labels for dynamic colors
         if color is None:
-            color = self.default_colors[0]
+            color = self.default_colors[self.slice_num % len(self.default_colors)]
         self.labels[hash(label_name)] = {"name": label_name, "color": color}
 
     def get_slice_labels(self, slice_location):
