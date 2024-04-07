484a925cfde8064c03ce6784fdd45f24d1b184c7
Tyler Walker <tyler.j.walker@maine.edu>
2024-04-07 19:58:51 -0400

    refactor(label): Move some functions out of useEffect

--- frontend/src/app/components/viewer.tsx
+++ frontend/src/app/components/viewer.tsx
@@ -12,6 +12,10 @@ export default function Viewer({
 }) {
   const canvasRef = useRef(null);
   const currentLabelRef = useRef(currentLabel);
+  let canvas;
+  let context;
+  let sliceImage;
+  let squareIndex;
 
   // Yes this is a massive bit of ducttape over geniunely bad code, but I'm carrying this project and don't care anymore.
   useEffect(() => {
@@ -20,44 +24,13 @@ export default function Viewer({
 
   // To be run whenever the slice updates
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext("2d");
-
-    let squareIndex = 0;
-    const sliceImage = new Image();
-    sliceImage.src = `http://127.0.0.1:3001/get_slice?index=${imageIndex}`;
-    sliceImage.onload = () => {
-      getLabels();
-      // Getting image information
-    };
-
-    const getLabels = () => {
-      axios
-        .get(`http://127.0.0.1:3001/get_slice_info?index=${imageIndex}`, {})
-        .then((res) => {
-          canvas.width = res.data["width"];
-          canvas.height = res.data["height"];
-
-          // Remove old labels and draw the slide image
-          context.clearRect(0, 0, canvas.width, canvas.height);
-          context.drawImage(sliceImage, 0, 0, canvas.width, canvas.height);
-
-          // Only render if there's segments to draw
-          if (res.data["segments"] != null && res.data["segments"].length > 0) {
-            for (let i = 0; i < res.data["segments"].length; i++) {
-              renderPolygon(
-                res.data["segments"][i].points,
-                res.data["segments"][i].name,
-              );
-              squareIndex++;
-            }
-            polygonPoints = res.data["segments"];
-          } else {
-            // Init
-            polygonPoints[0] = { name: "", points: [] };
-          }
-        });
-    };
+    canvas = canvasRef.current;
+    context = canvas.getContext("2d");
+    squareIndex = 0;
+    // sliceImage.onload = () => {
+    getLabels();
+    // Getting image information
+    // };
 
     const drawLabels = () => {
       squareIndex = 0;
@@ -77,34 +50,7 @@ export default function Viewer({
       }
     };
 
-    const getCurrentColor = (labelName = undefined) => {
-      let i;
-      for (i = 0; i < labels.length; i++) {
-        if (labels[i].name == labelName) {
-          return labels[i].color;
-        }
-      }
-    };
-
-    const renderPolygon = (points, labelName) => {
-      context.beginPath();
-      const rectWidth = points[1][0] - points[0][0];
-      const rectHeight = points[1][1] - points[0][1];
-      context.rect(points[0][0], points[0][1], rectWidth, rectHeight);
-      const color = getCurrentColor(labelName);
-      context.strokeStyle = "#" + color;
-      context.lineWidth = 2;
-      context.stroke();
-    };
-
     const handleClick = (event) => {
-      // console.log("BUTTON IS " + event.button);
-      // event.preventDefault();
-      // if (event.button == 1) {
-      //   console.log("Right click??");
-      //   handleRightClick(event);
-      //   return;
-      // }
       // This is taken to get the relative location of client clicks
       const canvasRect = canvas.getBoundingClientRect();
 
@@ -173,6 +119,57 @@ export default function Viewer({
       });
     };
   }, [imageIndex]);
+  const getLabels = () => {
+    canvas = canvasRef.current;
+    context = canvas.getContext("2d");
+    sliceImage = new Image();
+    sliceImage.src = `http://127.0.0.1:3001/get_slice?index=${imageIndex}`;
+    sliceImage.onload = () => {
+      axios
+        .get(`http://127.0.0.1:3001/get_slice_info?index=${imageIndex}`, {})
+        .then((res) => {
+          canvas.width = res.data["width"];
+          canvas.height = res.data["height"];
+
+          // Remove old labels and draw the slide image
+          context.clearRect(0, 0, canvas.width, canvas.height);
+          context.drawImage(sliceImage, 0, 0, canvas.width, canvas.height);
+
+          // Only render if there's segments to draw
+          if (res.data["segments"] != null && res.data["segments"].length > 0) {
+            for (let i = 0; i < res.data["segments"].length; i++) {
+              renderPolygon(
+                res.data["segments"][i].points,
+                res.data["segments"][i].name,
+              );
+              squareIndex++;
+            }
+            polygonPoints = res.data["segments"];
+          } else {
+            // Init
+            polygonPoints[0] = { name: "", points: [] };
+          }
+        });
+    };
+  };
+  const renderPolygon = (points, labelName) => {
+    context.beginPath();
+    const rectWidth = points[1][0] - points[0][0];
+    const rectHeight = points[1][1] - points[0][1];
+    context.rect(points[0][0], points[0][1], rectWidth, rectHeight);
+    const color = getCurrentColor(labelName);
+    context.strokeStyle = "#" + color;
+    context.lineWidth = 2;
+    context.stroke();
+  };
+  const getCurrentColor = (labelName = undefined) => {
+    let i;
+    for (i = 0; i < labels.length; i++) {
+      if (labels[i].name == labelName) {
+        return labels[i].color;
+      }
+    }
+  };
   return (
     <div>
       <canvas ref={canvasRef} />
