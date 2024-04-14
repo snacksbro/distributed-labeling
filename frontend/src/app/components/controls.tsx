4e9d45b9ce94912ca8bd526b0c2bd65e84c599ac
Tyler Walker <tyler.j.walker@maine.edu>
2024-04-14 14:19:20 -0400

    style(gui): Style DICOM navigation buttons

--- frontend/src/app/components/controls.tsx
+++ frontend/src/app/components/controls.tsx
@@ -7,14 +7,15 @@ export default function Controls({ sliceCount, imageIndex, updateImageIndex }) {
     updateImageIndex(imageIndex - 1);
   };
   return (
-    <div>
+    <div className="mt-[100%]">
       <input
         type="button"
         id="prev_slice"
         onClick={getPrevSlice}
         value="Prev"
+        className="bg-green-500 hover:bg-green-700 text-white px-5 text-xl border border-green-700 rounded"
       />
-      <span>
+      <span className="mx-5">
         {imageIndex} / {sliceCount}
       </span>
       <input
@@ -22,6 +23,7 @@ export default function Controls({ sliceCount, imageIndex, updateImageIndex }) {
         id="next_slice"
         onClick={getNextSlice}
         value="Next"
+        className="bg-green-500 hover:bg-green-700 text-white px-5 text-xl border border-green-700 rounded"
       />
     </div>
   );
