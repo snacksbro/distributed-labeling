6b7355d51ae84466ac0431d243699631d10b20b1
Tyler Walker <tyler.j.walker@maine.edu>
2024-04-14 02:23:09 -0400

    feat(gui): Get a REAL icon going and some padding

--- frontend/src/app/components/label.tsx
+++ frontend/src/app/components/label.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Label({ selectedLabel, labelIndex, labelContent }) {
   return (
     <li
-      className="bg-slate-50"
+      className="bg-slate-50 py-2 px-2"
       key={labelIndex}
       style={
         selectedLabel == labelContent.name
@@ -12,7 +12,18 @@ export default function Label({ selectedLabel, labelIndex, labelContent }) {
       }
     >
       <span style={{ color: "#" + labelContent.color, content: "" }}>
-        {"\x15"}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-6 h-6 inline"
+        >
+          <path
+            fillRule="evenodd"
+            d="M4.5 7.5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9Z"
+            clipRule="evenodd"
+          />
+        </svg>
       </span>
       {labelContent.name}
     </li>
