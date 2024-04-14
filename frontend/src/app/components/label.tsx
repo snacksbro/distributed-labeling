7dfcf1ef8ab9d372ad06b42068473a76d3086669
Tyler Walker <tyler.j.walker@maine.edu>
2024-04-14 01:50:07 -0400

    feat(gui): Make active label stand out more

--- frontend/src/app/components/label.tsx
+++ frontend/src/app/components/label.tsx
@@ -3,9 +3,12 @@ import React from "react";
 export default function Label({ selectedLabel, labelIndex, labelContent }) {
   return (
     <li
+      className="bg-slate-50"
       key={labelIndex}
       style={
-        selectedLabel == labelContent.name ? { border: "1px solid blue" } : {}
+        selectedLabel == labelContent.name
+          ? { backgroundColor: "rgb(203 213 225)" }
+          : {}
       }
     >
       <span style={{ color: "#" + labelContent.color, content: "" }}>
